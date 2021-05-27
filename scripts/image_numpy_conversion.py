23cd7292172b532d18be0f3a8f7bbff21109299d
AbsentAbsinthe <32835892+AbsentAbsinthe@users.noreply.github.com>
2021-05-27 19:57:46 +0100

    Spelling Change within py file

--- scripts/image_numpy_conversion.py
+++ scripts/image_numpy_conversion.py
@@ -5,10 +5,10 @@ import csv
 import numpy as np
 
 
-main_path = '../images\\'
+main_path = '../images/'
 paths = ['train','test','val']
 
 for p in paths:
     for file in os.listdir((main_path + p)):
         img = cv2.imread((main_path + p + '/' + file))
-        np.save('numpy_Images/' + p + '/' + file[0:-4] + '.npy', img)
+        np.save('numpy_images/' + p + '/' + file[0:-4] + '.npy', img)
