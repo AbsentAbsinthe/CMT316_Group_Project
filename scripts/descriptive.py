be84cb9bb4c355daea101e1bf6a94bfb34c00c95
parkyed <84448128+parkyed@users.noreply.github.com>
2021-05-29 11:19:23 +0100

    descriptive.py update
    I have removed my preprocessing.py file and incorporated all of my code into a single file, the descriptive.py file. This contains the processing of the text files into dataframes for the label data, and the production of the illustrations.

--- scripts/descriptive.py
+++ scripts/descriptive.py
@@ -29,6 +29,37 @@ lab_dir_csv = str(file_path)+'labels_csv/'  # labels data in csv files
 img_dir = str(file_path)+'images/'          # images
 rpt_dir = str(file_path)+'report/'          # report output
 
+
+# function to process the .txt files with label information into a Pandas DataFrame
+
+def label_dataframe(lab_dir):
+  """ file path -> pandas dataframe
+  Return a pandas dataframe containing the image annotation data contained in the
+  text files in the specified directory. for each line in a text file, add five items
+  to the row in the dataframe for that image, object class, x, y, w h of the bounding box
+  """
+  df_labels = []
+  for file in os.listdir(lab_dir):
+    row_labels = OrderedDict()
+    num_objects = 0
+    row_labels["fileID"] = str(file).split('.')[0]
+    with open(str(lab_dir) + str(file)) as file_in:
+      for i, line in enumerate(file_in):
+        line_list = line.split(sep=' ')
+        object_dict = OrderedDict()
+        object_dict["class_{}".format(i)] = line_list[0].strip()
+        object_dict["x_coord_{}".format(i)] = line_list[1].strip()
+        object_dict["y_coord_{}".format(i)] = line_list[2].strip()
+        object_dict["width_{}".format(i)] = line_list[3].strip()
+        object_dict["height_{}".format(i)] = line_list[4].strip()
+        row_labels.update(object_dict)
+        num_objects += 1
+    row_labels['num_objects'] = num_objects
+    df_labels.append(row_labels)
+  df_labels = pd.DataFrame(df_labels).sort_values(by='fileID')
+  return (df_labels)
+
+
 # function to count the number of images based on aspect ratio
 
 def find_image_sizes(image_dirs):
@@ -49,6 +80,17 @@ def find_image_sizes(image_dirs):
   sorted_image_size_desc = sorted(sorted_image_size, key=operator.itemgetter(1), reverse=False)
   return dict_image_size, sorted_image_size_desc
 
+
+# create dataframes  for train / val / test sets and save to csv to avoid the need to re-process later
+
+df_labels_train = label_dataframe(str(lab_dir)+'train/')
+df_labels_train.to_csv(str(lab_dir_csv)+'df_labels_train.csv')
+
+df_labels_val = label_dataframe(str(lab_dir)+'val/')
+df_labels_val.to_csv(str(lab_dir_csv)+'df_labels_val.csv')
+
+df_labels_test = label_dataframe(str(lab_dir)+'test/')
+df_labels_test.to_csv(str(lab_dir_csv)+'df_labels_test.csv')
 
 
 # importing saved csv files with label dataframe and concatonate for train+val+test for analysis
